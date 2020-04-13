14647284536db448285cea6e412485f9361ea8c4
Andrew Stewart <andrew@stwrt.ca>
2020-04-13 13:35:56 -0700

    Swap minus/underscore

--- keyboards/planck/keymaps/astewart/keymap.c
+++ keyboards/planck/keymaps/astewart/keymap.c
@@ -19,14 +19,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	* |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
 	* |Shift/( |    Z   |    X   |    C   |    V   |    B   |    N   |    M   |    ,   |    .   |    /   | Shift/)|
 	* |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
-	* |  Ctrl  |   Gui  |        |   Alt  |  Mods  |  Space | Enter  |  Bksp  |    [   |   ]    |    -   |    _   |
+	* |  Ctrl  |   Gui  |        |   Alt  |  Mods  |  Space | Enter  |  Bksp  |    [   |   ]    |    _   |    -   |
 	* `-----------------------------------------------------------------------------------------------------------'
 	*/
 	[QWERTY] = LAYOUT_planck_grid(
 		KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
 		CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 		KC_LSPO, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-		KC_LCTL, KC_LGUI, XXXXXXX, KC_LALT, MO(MOD), KC_SPC,  KC_ENT,  KC_BSPC, KC_LBRC, KC_RBRC, KC_MINS, KC_UNDS
+		KC_LCTL, KC_LGUI, XXXXXXX, KC_LALT, MO(MOD), KC_SPC,  KC_ENT,  KC_BSPC, KC_LBRC, KC_RBRC, KC_UNDS, KC_MINS
 	),
 
 	/* Mods
